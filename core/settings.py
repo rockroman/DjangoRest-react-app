476c069e7f73f3270610f00fcdf218cabe77c28a
Roman Rakic <rock.rakic@gmail.com>
2023-07-27 15:54:48 +0100

    install dango REST and create serializers with view

--- core/settings.py
+++ core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "blog_api",
     "blog",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog_api.urls"), name="blog_api"),
+    path("api/", include("blog_api.urls"), name="blog_api"),
     path("", include("blog.urls"), name="blog"),
 ]
